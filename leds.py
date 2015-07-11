28de5634984228cb5e627ef2ca1de5c12f266dc2
Saul Rodriguez <sinsonido@gmail.com>
2015-07-11 23:34:46 +0200

    clean py

--- leds.py
+++ leds.py
@@ -1,19 +1,8 @@
 #!/usr/bin/env python
 
-from imapclient import IMAPClient
-import time
-
 import RPi.GPIO as GPIO
 
 DEBUG = True
-
-HOSTNAME = 'imap.gmail.com'
-USERNAME = 'your username here'
-PASSWORD = 'your password here'
-MAILBOX = 'Inbox'
-
-NEWMAIL_OFFSET = 1   # my unread messages never goes to zero, yours might
-MAIL_CHECK_FREQ = 60 # check mail every 60 seconds
 
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BCM)
@@ -22,21 +11,8 @@ RED_LED = 23
 GPIO.setup(GREEN_LED, GPIO.OUT)
 GPIO.setup(RED_LED, GPIO.OUT)
 
-def loop():
-    server = IMAPClient(HOSTNAME, use_uid=True, ssl=True)
-    server.login(USERNAME, PASSWORD)
-
-    if DEBUG:
-        print('Logging in as ' + USERNAME)
-        select_info = server.select_folder(MAILBOX)
-        print('%d messages in INBOX' % select_info['EXISTS'])
-
-    folder_status = server.folder_status(MAILBOX, 'UNSEEN')
-    newmails = int(folder_status['UNSEEN'])
-
-    if DEBUG:
-        print "You have", newmails, "new emails!"
-
+def init():
+    
     if newmails > NEWMAIL_OFFSET:
         GPIO.output(GREEN_LED, True)
         GPIO.output(RED_LED, False)
@@ -49,7 +25,6 @@ def loop():
 if __name__ == '__main__':
     try:
         print 'Press Ctrl-C to quit.'
-        while True:
-            loop()
+        init()
     finally:
         GPIO.cleanup()
